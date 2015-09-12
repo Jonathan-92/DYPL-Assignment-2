eabdb9def40338e5747ada619c794fc499bed7e3
Jonathan-92 <jonte790@gmail.com>
2015-09-12 16:46:44 +0200

    removed ignored files

--- old_code.rb
+++
@@ -1,9 +0,0 @@
-
-#in code_generation.rb in Model.define_attributes()
-if eval("#{constraints.join(' && ')}")
-            @#{name} = #{name}
-          else
-            raise 'Wrongful update for #{name}'
-          end
-          
-          
